65682659d39fbb42a55e11a6f584b21ffbc9a68e
ragkar <ragnhild.karlsson@gmail.com>
2013-07-24 11:22:38 +0200

    firstversion

--- gen/com/example/goodbyelamb/R.java
+++ gen/com/example/goodbyelamb/R.java
@@ -21,28 +21,54 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int chewing_sheep=0x7f020000;
+        public static final int curly_goat=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int staring_sheep=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int edit_message=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int button1=0x7f080000;
+        public static final int check=0x7f080005;
+        public static final int counted_all=0x7f080002;
+        public static final int edit_message=0x7f080003;
+        public static final int label=0x7f080004;
+        public static final int textView1=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_display_message=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_counted_all=0x7f030000;
+        public static final int activity_counting=0x7f030001;
+        public static final int activity_display_message=0x7f030002;
+        public static final int activity_main=0x7f030003;
+        public static final int activity_start_screen=0x7f030004;
+        public static final int rowlayout=0x7f030005;
     }
     public static final class menu {
-        public static final int display_message=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int counted_all=0x7f070000;
+        public static final int counting=0x7f070001;
+        public static final int display_message=0x7f070002;
+        public static final int main=0x7f070003;
+        public static final int start_screen=0x7f070004;
+        public static final int your_animal_lists=0x7f070005;
     }
     public static final class string {
         public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
         public static final int button_send=0x7f050002;
+        public static final int counted_all_count_again=0x7f05000f;
+        public static final int counting_activity_count=0x7f050010;
+        public static final int counting_counted_all=0x7f05000d;
+        public static final int counting_sheeps_label=0x7f05000c;
         public static final int edit_message=0x7f050001;
         public static final int hello_world=0x7f050006;
+        public static final int start_screen_count_us_in=0x7f050008;
+        public static final int title_activity_counted_all=0x7f05000e;
+        public static final int title_activity_counting=0x7f05000b;
         public static final int title_activity_display_message=0x7f050005;
         public static final int title_activity_main=0x7f050004;
+        public static final int title_activity_start_screen=0x7f050007;
+        public static final int title_activity_your_animal_lists=0x7f050009;
+        public static final int your_animal_lists=0x7f05000a;
     }
     public static final class style {
         /** 
